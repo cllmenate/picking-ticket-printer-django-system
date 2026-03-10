d67010d52932283d9c1915a73e23ac469656fe37
Gustavo Brandão <sm70plus@hotmail.com>
2026-03-10 11:17:30 -0300

    feat: Initialize Django project with Docker, Nginx, and the core `ticket_printer` application structure.

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -54,3 +56,8 @@ urlpatterns = [
     path("imports/", include("apps.imports.urls")),
     path("printer/", include("apps.ticket_printer.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )

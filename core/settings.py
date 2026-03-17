e64e0f351677bb1f1d1691100470ef487c375ae4
Nattam Pereira <nattam.pereira@gmail.com>
2026-03-17 14:46:33 -0300

    fix: adiciona variável de ambiente para CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -57,6 +57,11 @@ DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
+CSRF_TRUSTED_ORIGINS = env.list(
+    "CSRF_TRUSTED_ORIGINS",
+    default=["http://localhost:8000", "http://127.0.0.1:8000"]
+)
+
 
 # Application definition
 

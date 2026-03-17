fa1c2c89de4b30c55ddb658a5fa1d51b660ae8e8
Nattam Pereira <nattam.pereira@gmail.com>
2026-03-17 13:35:27 -0300

    fix: Database settings

--- core/settings.py
+++ core/settings.py
@@ -133,8 +133,14 @@ if os.getenv("POSTGRES_DB"):
             "NAME": os.getenv("POSTGRES_DB"),
             "USER": os.getenv("POSTGRES_USER"),
             "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "HOST": os.getenv("POSTGRES_HOST"),
-            "PORT": os.getenv("POSTGRES_PORT"),
+            "HOST": os.getenv(
+                "POSTGRES_HOST",
+                os.getenv("DB_HOST", os.getenv("PGHOST"))
+            ),
+            "PORT": os.getenv(
+                "POSTGRES_PORT",
+                os.getenv("DB_PORT", os.getenv("PGPORT", "5432"))
+            ),
         }
     }
 else:
